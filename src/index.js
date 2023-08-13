f30705a8b1b92b01a2e2156df73cc1a949654150
Paez99 <javierpaez012899@gmail.com>
2023-08-13 16:25:51 -0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import alumnosRoutes from './routes/alumnos.routes.js'
 import trabajoFinalRoutes from './routes/trabajofinal.routes.js'
+import cors from 'cors'
+
 
 const app = express()
 
+app.use(cors())
 app.use(alumnosRoutes)
 app.use(trabajoFinalRoutes)
 
